0c2857347753c6d9b51f065580d9703f8d0f0d68
MdR <mrslygrin@gmail.com>
2024-05-15 15:24:37 +0500

    chore: add description to the course card

--- src/components/course-card/index.tsx
+++ src/components/course-card/index.tsx
@@ -1,31 +1,37 @@
-import { Bookmark } from 'lucide-react';
+import { Bookmark } from "lucide-react";
 
 export const CourseCard = () => {
   return (
-    <div className="flex flex-col bg-white rounded-lg border border-gray-200 overflow-hidden transition-all hover:shadow-md">
+    <div className="mx-auto flex max-w-[400px] flex-col overflow-hidden rounded-lg border border-gray-200 bg-white transition-all hover:shadow-md sm:max-w-none">
       <div>
         <img
-          className="w-full h-40 object-cover"
+          className="h-40 w-full object-cover"
           src="https://source.unsplash.com/random"
           alt="courseimage "
         />
       </div>
 
-      <div className="p-4 flex flex-1 flex-col">
-        <div className="flex flex-1 gap-4 mb-24">
-          <h3 className="text-lg font-semibold mr-auto">Course Title</h3>
+      <div className="flex flex-1 flex-col p-4">
+        <div className="flex flex-1 gap-4">
+          <h3 className="mr-auto text-lg font-semibold">Course Title</h3>
           <Bookmark className="text-gray-300" />
         </div>
+
+        <p className="mb-10 mt-3 line-clamp-2 text-sm text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam consequatur cupiditate, ex
+          molestiae laudantium minus quod in consequuntur ab, aliquam sequi ratione possimus qui
+          dolorem deleniti? Maiores quibusdam hic natus.
+        </p>
 
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2">
             <img
-              className="w-8 h-8 rounded-full shrink-0"
+              className="h-8 w-8 shrink-0 rounded-full"
               src="https://source.unsplash.com/random"
               alt="avatar"
             />
             <div className="overflow-hidden">
-              <p className="text-sm/4 text-gray-500 break-words line-clamp-1">Instructor Name</p>
+              <p className="line-clamp-1 break-words text-sm/4 text-gray-500">Instructor Name</p>
             </div>
           </div>
 
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,9 @@
-import { CourseCard } from '@/components/course-card';
+import { CourseCard } from "@/components/course-card";
 
 export const Main = () => {
   return (
     <>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
         <CourseCard />
         <CourseCard />
         <CourseCard />
